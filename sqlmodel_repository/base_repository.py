844b0878003616fa63b0c278a88c0e675a273f4c
Yannic Schröer <yannic@schroeer.tech>
2023-04-10 12:50:48 +0200

    logging and tests added

--- sqlmodel_repository/base_repository.py
+++ sqlmodel_repository/base_repository.py
@@ -1,11 +1,15 @@
 from abc import ABC, abstractmethod
 from functools import lru_cache
+from logging import Logger, getLogger
 from typing import Generic, Type, TypeVar, get_args
 
 from sqlalchemy.orm import Session
+from sqlalchemy.sql.elements import ColumnClause
+from structlog import WriteLogger
 
 from sqlmodel_repository.entity import SQLModelEntity
 from sqlmodel_repository.exceptions import CouldNotCreateEntityException, CouldNotDeleteEntityException, EntityDoesNotPossessAttributeException, EntityNotFoundException
+from sqlmodel_repository.logger import sqlmodel_repository_logger
 
 GenericEntity = TypeVar("GenericEntity", bound=SQLModelEntity)
 
@@ -14,9 +18,24 @@ class BaseRepository(Generic[GenericEntity], ABC):
     """Abstract base class for all repositories"""
 
     entity: Type[GenericEntity]
+    logger: WriteLogger
+    sensitive_attribute_keys: list[str] = []
+    _default_excluded_keys = ["_sa_instance_state"]
 
-    def __init__(self):
+    def __init__(self, logger: WriteLogger | None = None, sensitive_attribute_keys: list[str] | None = None):
+        """Initializes the repository
+
+        Args:
+            logger (Logger, optional): The logger to use. Defaults to None and will use the default logger.
+            sensitive_attribute_keys (list[str], optional): A list of attributes that should be excluded from the logs. Defaults to None.
+
+        Notes:
+            - The default logger is a structlog logger that logs in JSON format.
+            - The default exclusion list (_default_excluded_keys) is ["_sa_instance_state"] which is a default SQLAlchemy attribute that is added to all entities. You may override this.
+        """
         self.entity = self._entity_class()
+        self.logger = logger if logger is not None else sqlmodel_repository_logger
+        self.sensitive_attribute_keys = sensitive_attribute_keys if sensitive_attribute_keys is not None else []
 
     @abstractmethod
     def get_session(self) -> Session:
@@ -40,6 +59,8 @@ class BaseRepository(Generic[GenericEntity], ABC):
             This method must use the same context to fetch and update the entity. Otherwise its detached and may not be updated.
         """
         session = self.get_session()
+        self._emit_log("Updating", entities=[entity], **kwargs)
+
         entity = self._get(entity_id=entity.id)
 
         for key, value in kwargs.items():
@@ -66,6 +87,7 @@ class BaseRepository(Generic[GenericEntity], ABC):
         """
 
         session = self.get_session()
+        self._emit_log("Batch updating", entities=entities, **kwargs)
 
         for entity in entities:
             for key, value in kwargs.items():
@@ -95,13 +117,15 @@ class BaseRepository(Generic[GenericEntity], ABC):
             EntityNotFoundException: If the entity was not found in the database
         """
         session = self.get_session()
+        self._emit_log("Getting", id=entity_id)
+
         result = session.query(self.entity).filter(self.entity.id == entity_id).one_or_none()
         if result is None:
             raise EntityNotFoundException(f"Entity {GenericEntity.__name__} with ID {entity_id} not found")
         return result
 
     # pylint: disable=dangerous-default-value
-    def _get_batch(self, filters: list = []) -> list[GenericEntity]:
+    def _get_batch(self, filters: list | None = None) -> list[GenericEntity]:
         """Retrieves a list of entities from the database that match the specified filters.
 
         Args:
@@ -111,6 +135,11 @@ class BaseRepository(Generic[GenericEntity], ABC):
             list[GenericEntity]: A list of GenericEntity objects that match the specified filters.
         """
         session = self.get_session()
+        filters = filters if filters is not None else []
+
+        # TODO: Add (MEANINGFUL!) filters to log. This is a bit tricky because filters is a list of ColumnClause objects and the type is not correctly defined within SQLModel.
+        self._emit_log("Batch get")
+
         result = session.query(self.entity).filter(*filters).all()
         return result
 
@@ -127,6 +156,7 @@ class BaseRepository(Generic[GenericEntity], ABC):
             CouldNotCreateEntityException: If there was an error inserting the entity into the database.
         """
         session = self.get_session()
+        self._emit_log("Creating", entities=[entity])
 
         try:
             session.add(entity)
@@ -147,6 +177,8 @@ class BaseRepository(Generic[GenericEntity], ABC):
             list[GenericEntity]: The objects that were added to the database, with any auto-generated fields populated.
         """
         session = self.get_session()
+        self._emit_log("Batch creating", entities=entities)
+
         try:
             for entity in entities:
                 session.add(entity)
@@ -173,6 +205,8 @@ class BaseRepository(Generic[GenericEntity], ABC):
             DatabaseError: If there was an error deleting the entity from the database.
         """
         session = self.get_session()
+        self._emit_log("Deleting", entities=[entity])
+
         try:
             session.delete(entity)
             session.commit()
@@ -191,6 +225,7 @@ class BaseRepository(Generic[GenericEntity], ABC):
             CouldNotDeleteEntityException: If there was an error deleting the entities from the database.
         """
         session = self.get_session()
+        self._emit_log("Batch deleting", entities=entities)
 
         try:
             for entity in entities:
@@ -200,6 +235,36 @@ class BaseRepository(Generic[GenericEntity], ABC):
         except Exception as exception:
             session.rollback()
             raise CouldNotDeleteEntityException from exception
+
+    def _safe_kwargs(self, prefix: str = "", **kwargs) -> dict[str, str]:
+        """Filters out sensitive attributes from the log kwargs
+
+        Args:
+            **kwargs: The key-value pairs to filter
+
+        Returns:
+            dict[str, str]: The filtered key-value pairs
+        """
+        excluded_keys = [*self.sensitive_attribute_keys, *self._default_excluded_keys]
+        return {f"{prefix}{key}": value for key, value in kwargs.items() if key not in excluded_keys}
+
+    def _emit_log(self, operation: str, entities: list[GenericEntity] | None = None, **kwargs) -> None:
+        """Emits a log message for the specified event
+
+        Args:
+            message (str): The log message to emit
+            entities (list[GenericEntity] | None): A list of entities to include in the log message. Default is None.
+            **kwargs: Additional key-value pairs to include in the log message.
+        """
+        entities = entities or []
+        try:
+            entity_payloads = [self._safe_kwargs(**entity.dict()) for entity in entities]
+            entity_log: dict = {**entity_payloads[0]} if len(entity_payloads) == 1 else {"payload": entity_payloads}
+            kwargs_log: dict = self._safe_kwargs(prefix="kwarg_", **kwargs)  # Prefix is necessary to avoid conflicts with entity attributes
+            self.logger.debug(f"{operation} {self.entity.__name__}", **entity_log, **kwargs_log)
+        except Exception as exception:  # pylint: disable=broad-except:
+            # We want to catch all exceptions here. Logs must be written by all means. It's no silent passing and thereby acceptable.
+            self.logger.warning(f"Could not emit log for {operation} {self.entity.__name__}", exception=exception)  # type: ignore TODO: fix this
 
     @classmethod
     @lru_cache(maxsize=1)
---
+++ tests/unit/test_logs.py
@@ -0,0 +1,204 @@
+from typing import Generator
+from unittest.mock import MagicMock
+import pytest
+from sqlalchemy.sql.elements import ColumnClause
+from sqlmodel import col
+from structlog import WriteLogger
+from sqlmodel_repository.entity import SQLModelEntity
+from sqlmodel_repository import BaseRepository
+import json
+
+
+class TestLogEntity(SQLModelEntity, table=True):
+    string_attribute: str
+    integer_attribute: int
+    password: str
+
+
+class MockBaseRepository(BaseRepository[TestLogEntity]):  # type: ignore
+    def get_session(self):
+        return MagicMock()
+
+
+# pylint: disable=protected-access
+class TestLogs:
+    """Test the logging functionality of the BaseRepository class."""
+
+    @pytest.fixture
+    def base_repository(self) -> Generator[BaseRepository, None, None]:
+        """Return a BaseRepository instance.""" ""
+        yield MockBaseRepository(sensitive_attribute_keys=["password"])
+
+    @pytest.fixture
+    def entity(self) -> TestLogEntity:
+        """Return a TestLogEntity instance."""
+        return TestLogEntity(id=1, string_attribute="test_string", integer_attribute=1, password="test_password")
+
+    def _test_attributes(self, values_to_check: dict, log_entry: dict, base_repository: BaseRepository):
+        for attribute_key in values_to_check.keys():
+            if attribute_key not in [*base_repository.sensitive_attribute_keys, *base_repository._default_excluded_keys]:
+                assert log_entry.get(attribute_key) == values_to_check.get(attribute_key)
+
+    def get_log_entry(self, caplog, message_beginning: str) -> dict:
+        """Return the log entry for a given message beginning."""
+        for record in caplog.records:
+            _json = json.loads(record.message)
+            if _json.get("event").startswith(message_beginning):
+                return _json
+        raise ValueError(f"No log entry found for message beginning: {message_beginning}")
+
+    def test_set_logger(self):
+        """Test that the logger can be set."""
+        logger = WriteLogger()
+        repository = MockBaseRepository(logger)
+        assert repository.logger == logger
+
+    def test_emit_log(self, caplog, base_repository: BaseRepository, entity: TestLogEntity):
+        """Test that the log is emitted."""
+        base_repository._emit_log("test_event", entities=[entity], **entity.dict())
+        log_entry = self.get_log_entry(caplog, "test_event")
+        self._test_attributes(entity.dict(), log_entry, base_repository)
+
+    def test_emit_log_silent_raise_exception(self, caplog, base_repository: BaseRepository, entity: TestLogEntity):
+        """Test that the log is emitted even if an exception is raised."""
+        base_repository._emit_log("test_event", entities=["entity"])  # type: ignore
+        log_entry = self.get_log_entry(caplog, f"Could not emit log for test_event {entity.__class__.__name__}")
+        assert log_entry
+
+    def test_set_sensitive_attributes(self):
+        """Test that the sensitive attributes are set."""
+        repository = MockBaseRepository(sensitive_attribute_keys=["password"])
+        assert repository.sensitive_attribute_keys == ["password"]
+
+    def test_get_log_kwargs_entity(self):
+        """Test that the log kwargs are returned."""
+        repository = MockBaseRepository(sensitive_attribute_keys=["password"])
+
+        entity = {"id": 1, "string_attribute": "test_string", "integer_attribute": 1, "password": "test_password"}
+        assert repository._safe_kwargs(**entity) == {
+            "id": entity.get("id"),
+            "string_attribute": entity.get("string_attribute"),
+            "integer_attribute": entity.get("integer_attribute"),
+        }
+
+    def test_get_log_kwargs_entity_without_exlcuded_key(self):
+        """Test that the log kwargs are returned."""
+        repository = MockBaseRepository()
+        entity = {"id": 1, "string_attribute": "test_string", "integer_attribute": 1, "password": "test_password"}
+        assert repository._safe_kwargs(**entity) == {
+            "id": entity.get("id"),
+            "string_attribute": entity.get("string_attribute"),
+            "integer_attribute": entity.get("integer_attribute"),
+            "password": entity.get("password"),
+        }
+
+    def test_update_log(self, base_repository: BaseRepository, entity: TestLogEntity, caplog):
+        """Test that _update logs the correct information."""
+        # Copy the entity attributes to check against the log entry
+        values_to_check = entity.dict()
+
+        # Update the entity
+        base_repository._update(entity, string_attribute="new_string")
+        log_entry = self.get_log_entry(caplog=caplog, message_beginning="Updating")
+        assert log_entry
+
+        # Check that event is logged
+        assert log_entry.get("event") == f"Updating {entity.__class__.__name__}"
+
+        # Check that the entity attributes are logged
+        self._test_attributes(values_to_check=values_to_check, log_entry=log_entry, base_repository=base_repository)
+
+        # Check that the kwarg is logged
+        assert log_entry.get("kwarg_string_attribute") == "new_string"
+
+    def test_update_batch_log_kwargs(self, base_repository: BaseRepository, entity: TestLogEntity, caplog):
+        """Test that _update_batch logs the correct information."""
+        # Copy the entity attributes to check against the log entry
+        values_to_check = entity.dict()
+
+        # Update the entities
+        base_repository._update_batch([entity], string_attribute="new_string")
+        log_entry = self.get_log_entry(caplog=caplog, message_beginning="Batch updating")
+        assert log_entry
+
+        # Check that event is logged
+        assert log_entry.get("event") == f"Batch updating {entity.__class__.__name__}"
+
+        # Check that the entity attributes are logged
+        self._test_attributes(values_to_check=values_to_check, log_entry=log_entry, base_repository=base_repository)
+
+        # Check that the kwarg is logged
+        assert log_entry.get("kwarg_string_attribute") == "new_string"
+
+    def test_get_log(self, base_repository: BaseRepository, entity: TestLogEntity, caplog):
+        """Test the the _get method logs the correct information."""
+        # Get the entity
+        base_repository._get(entity.id)
+        log_entry = self.get_log_entry(caplog=caplog, message_beginning="Getting")
+        assert log_entry
+
+        # Check that event is logged
+        assert log_entry.get("event") == f"Getting {entity.__class__.__name__}"
+        assert log_entry.get("kwarg_id") == entity.id
+
+    def test_get_batch_log(self, base_repository: BaseRepository, entity: TestLogEntity, caplog):
+        """Test the the _get_batch method logs the correct information."""
+        # Get the entity
+        base_repository._get_batch([col(TestLogEntity.id) == entity.id])
+        log_entry = self.get_log_entry(caplog=caplog, message_beginning="Batch get")
+        assert log_entry
+
+        # Check that event is logged
+        assert log_entry.get("event") == f"Batch get {entity.__class__.__name__}"
+
+    def test_create_log(self, base_repository: BaseRepository, entity: TestLogEntity, caplog):
+        """Test that the _create method logs the correct information."""
+        # Create the entity
+        base_repository._create(entity)
+        log_entry = self.get_log_entry(caplog=caplog, message_beginning="Creating")
+        assert log_entry
+
+        # Check that event is logged
+        assert log_entry.get("event") == f"Creating {entity.__class__.__name__}"
+
+        # Check that the entity attributes are logged
+        self._test_attributes(values_to_check=entity.dict(), log_entry=log_entry, base_repository=base_repository)
+
+    def test_create_batch_log(self, base_repository: BaseRepository, entity: TestLogEntity, caplog):
+        """Test that the _create_batch method logs the correct information."""
+        # Create the entity
+        base_repository._create_batch([entity])
+        log_entry = self.get_log_entry(caplog=caplog, message_beginning="Batch creating")
+        assert log_entry
+
+        # Check that event is logged
+        assert log_entry.get("event") == f"Batch creating {entity.__class__.__name__}"
+
+        # Check that the entity attributes are logged
+        self._test_attributes(values_to_check=entity.dict(), log_entry=log_entry, base_repository=base_repository)
+
+    def test_delete_log(self, base_repository: BaseRepository, entity: TestLogEntity, caplog):
+        """Test that the _delete method logs the correct information."""
+        # Delete the entity
+        base_repository._delete(entity)
+        log_entry = self.get_log_entry(caplog=caplog, message_beginning="Deleting")
+        assert log_entry
+
+        # Check that event is logged
+        assert log_entry.get("event") == f"Deleting {entity.__class__.__name__}"
+
+        # Check that the entity attributes are logged
+        self._test_attributes(values_to_check=entity.dict(), log_entry=log_entry, base_repository=base_repository)
+
+    def test_delete_batch_log(self, base_repository: BaseRepository, entity: TestLogEntity, caplog):
+        """Test that the _delete_batch method logs the correct information."""
+        # Delete the entity
+        base_repository._delete_batch([entity])
+        log_entry = self.get_log_entry(caplog=caplog, message_beginning="Batch deleting")
+        assert log_entry
+
+        # Check that event is logged
+        assert log_entry.get("event") == f"Batch deleting {entity.__class__.__name__}"
+
+        # Check that the entity attributes are logged
+        self._test_attributes(values_to_check=entity.dict(), log_entry=log_entry, base_repository=base_repository)
